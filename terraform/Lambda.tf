012ab24d4e41ba183405104f5ae2ed0d5f83e591
Bharath Reddy Yalaka <bharathreddyyalaka@Bharaths-Air.lan>
2023-12-21 00:24:39 -0800

    modified jenkinsfile and lambda.tf

--- terraform/Lambda.tf
+++ terraform/Lambda.tf
@@ -26,7 +26,7 @@ resource "aws_iam_role" "iam_for_lambda" {
 resource "aws_lambda_function" "demo_lambda" {
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
-  filename      = "/Users/bharathreddyyalaka/Desktop/myproject/target/myproject-1.0-SNAPSHOT.jar"
+  filename      = "/Users/bharathreddyyalaka/.m2/repository/com/example/myproject/1.0-SNAPSHOT/myproject-1.0-SNAPSHOT.jar"
   function_name = "${local.project_name}${var.env_suffix}"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "com.example.App::handleRequest"
